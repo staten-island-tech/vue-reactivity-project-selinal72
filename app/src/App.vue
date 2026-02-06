f01eee7b6fd2fa83bd7d93f91ebcad7a2e4625a3
selinal72 <selinal72@nycstudents.net>
2026-02-06 14:02:19 -0500

    spend money game start

---
+++ app/src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>spend all of elon musk's money</h1>
+  <h2>841,100,000,000 us dollaroos</h2>
+</template>
+
+<style scoped></style>
---
+++ app/src/components/ShoppingItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>{{ product.name }}</h3>
+    <h4>{{ product.price }}</h4>
+    <button>buy</button>
+    <button>sell</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
